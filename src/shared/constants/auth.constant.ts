faa33c9411abe081da1059067f97e818dbc2d313
TranNguyenHaiDang <trannguyenhaidangpy@gmai.com>
2026-01-19 00:31:34 +0700

    create shared module

---
+++ src/shared/constants/auth.constant.ts
@@ -0,0 +1,13 @@
+export const REQUEST_USER_KEY = 'user'
+export const AuthType = {
+  Bearer: 'Bearer',
+  None: 'None',
+  APIKey: 'APIKey',
+} as const
+export type AuthTypeType = (typeof AuthType)[keyof typeof AuthType]
+
+export const ConditionGuard = {
+  And: 'and',
+  Or: 'or',
+} as const
+export type ConditionGuardType = (typeof ConditionGuard)[keyof typeof ConditionGuard]
