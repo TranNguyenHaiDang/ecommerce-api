7f27d576ce50043e137972064ea102ed900e964e
TranNguyenHaiDang <trannguyenhaidangpy@gmai.com>
2026-01-20 17:58:17 +0700

    refactor register feature

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -3,35 +3,28 @@ import { HashingService } from '@/shared/services/hashing.service'
 import { PrismaService } from '@/shared/services/prisma.service'
 import { ConflictException, Injectable } from '@nestjs/common'
 import { RolesService } from './roles.service'
-import { RegisterBodyDTO } from './auth.dto'
+import { RegisterBodyType } from './auth.model'
+import { AuthRepository } from './auth.repo'
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly prismaService: PrismaService,
     private readonly hashingService: HashingService,
     private readonly rolesService: RolesService,
+    private readonly authRepository: AuthRepository,
   ) {}
 
-  async register(body: RegisterBodyDTO) {
+  async register(body: RegisterBodyType) {
     try {
       const clientRoleId = await this.rolesService.getClientRoleId()
       const hashedPassword = await this.hashingService.hash(body.password)
-      const user = await this.prismaService.user.create({
-        data: {
-          email: body.email,
-          password: hashedPassword,
-          name: body.name,
-          phoneNumber: body.phoneNumber,
-          roleId: clientRoleId,
-        },
-        //không cho trả về
-        omit: { 
-          password: true,
-          totpSecret: true,
-        },
+      return await this.authRepository.createUser({
+        email: body.email,
+        name: body.name,
+        phoneNumber: body.phoneNumber,
+        password: hashedPassword,
+        roleId: clientRoleId,
       })
-      return user
     } catch (error) {
       if (isUniqueConstraintPrismaError(error)) {
         throw new ConflictException('Email đã tồn tại')
